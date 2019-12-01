aaced52264d0cce9e1785de041f3a880f65468c5
Влад <vlabster@mail.ru>
2019-12-01 13:47:34 +0300

    added new logo

--- profile.php
+++ profile.php
@@ -27,65 +27,55 @@
 			<div class="circle">
 
 			</div>
-			<div class="plus">
+			<!-- <div class="plus">
 				<div class="input__wrapper">
 					<input name="file" type="file" name="file" id="input__file" class="input input__file" multiple>
 					<label for="input__file" class="input__file-button">
 						<span class="input__file-icon-wrapper"><img class="input__file-icon" src="logos/day.png" alt="Выбрать файл" width="25"></span>
 					</label>
 				</div>
-			</div>
+			</div> -->
 			<div class="fio">
 				<? echo ("$name $surname"); ?>
 			</div>
 		</div>
 		<div class="check">
 			<?
-			include 'actions/getMyTours.php';
-			while ($row = $res->fetch_assoc()) {
-				$name = $row['name'];
-				$surname = $row['surname'];
-				$parse->get_tpl('temp/tourCard.tpl'); //Файл который мы будем парсить
-				$FI = $surname . " " . $name;
-				$parse->set_tpl('{FI}', "$FI");
+			// include 'actions/getMyTours.php';
+			// while ($row = $res->fetch_assoc()) {
+			// 	$name = $row['name'];
+			// 	$surname = $row['surname'];
+			// 	$parse->get_tpl('temp/tourCard.tpl'); //Файл который мы будем парсить
+			// 	$FI = $surname . " " . $name;
+			// 	$parse->set_tpl('{FI}', "$FI");
 
-				$email = $row['email'];
-				$parse->set_tpl('{EMAIL}', "$email");
+			// 	$email = $row['email'];
+			// 	$parse->set_tpl('{EMAIL}', "$email");
 
-				$hotelName = $row['hotel'];
-				$parse->set_tpl('{HOTELNAME}', "$hotelName");
+			// 	$hotelName = $row['hotel'];
+			// 	$parse->set_tpl('{HOTELNAME}', "$hotelName");
 
-				$place = $row['attractions'];
-				$parse->set_tpl('{PLACE}', "$place");
+			// 	$place = $row['attractions'];
+			// 	$parse->set_tpl('{PLACE}', "$place");
 
-				$ticket = $row['ticket'];
-				$parse->set_tpl('{TICKET}', "$ticket");
+			// 	$ticket = $row['ticket'];
+			// 	$parse->set_tpl('{TICKET}', "$ticket");
 
-				$photo = $row['photo'];
-				$parse->set_tpl('{PHOTO}', $photo);
+			// 	$photo = $row['photo'];
+			// 	$parse->set_tpl('{PHOTO}', $photo);
 
-				$parse->tpl_parse(); //Парсим
+			// 	$parse->tpl_parse(); //Парсим
 
-				echo ($parse->template);
-			}
+			// 	echo ($parse->template);
+			// }
 			?>
+			<img src="img/logos/card.png" alt="" width=335px height="=100px" style="margin-top: 2%;">
 		</div>
 		<div class="place">
 			<div class="btn">
 				Добавить человека
 			</div>
-			<div class="add">
-				Леонид
-			</div>
-			<div class="add1">
-				Стив
-			</div>
-			<div class="add2">
-				Рикардо
-			</div>
-			<div class="add3">
-				Васильевич
-			</div>
+			<img src="img/logos/ludi.png" alt="" width=350px height="=24px" style="margin-left: -60%; margin-bottom: -42%">
 		</div>
 		<div class="game">
 			<?
